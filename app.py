be9b8235eca6c056d672b3e2d74d3aed026fe5a0
salem050 <salem05@umd.edu>
2026-09-10 20:45:49 -0400

    Added personalized greeting and Flask requirements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World From Salem Eshete!'
+    return 'Hello World From Salem Eshete! I am adding my first code change.'
 
 
 if __name__ == '__main__':
