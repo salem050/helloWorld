b6c46eee677b94caabff675ba0127a6959aca6ad
salem050 <salem05@umd.edu>
2026-09-10 20:58:41 -0400

    Added hello route for HTML page

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World From Salem Eshete! I am adding my first code change.'
+
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 
 if __name__ == '__main__':
